a3341c48aeea431b7bec5dda3062ce29e1c370ab
Juan García <garciabrizjuan@gmail.com>
2022-05-08 13:49:02 +0200

    Added input handler, player locomotion

--- Assets/Scripts/PlayerLocomotion.cs
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -2,17 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLocomotion : MonoBehaviour
+namespace SL
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerLocomotion : MonoBehaviour
     {
-        
-    }
+        Transform cameraObject;
+        InputHandler inputHandler;
+        Vector3 moveDirection;
 
-    // Update is called once per frame
-    void Update()
-    {
+        [HideInInspector] public Transform myTransform;
+
+        public Rigidbody rigidBody;
+        public GameObject normalCamera;
+
+        [Header("Stats")]
+        [SerializeField] float movementSpeed = 5;
+        [SerializeField] float rotationSpeed = 10;
+
+        void Start()
+        {
+            rigidBody = GetComponent<Rigidbody>();
+            inputHandler = GetComponent<InputHandler>();
+            cameraObject = Camera.main.transform;
+            myTransform = transform;
+        }
+
+        void Update()
+        {
+            float delta = Time.deltaTime;
+
+            inputHandler.TickInput(delta);
+
+            moveDirection = cameraObject.forward * inputHandler.vertical;
+            moveDirection += cameraObject.right * inputHandler.horizontal;
+            moveDirection.Normalize();
+
+            float speed = movementSpeed;
+            moveDirection *= speed;
+
+            Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
+            rigidBody.velocity = projectedVelocity;
+        }
+
+        #region Movement
         
+        Vector3 normalVector;
+        Vector3 targetPosition;
+
+        private void HandleRotation(float delta)
+        {
+            Vector3 targetDirection = Vector3.zero;
+            float moveOverride = inputHandler.moveAmount;
+
+            targetDirection = cameraObject.forward * inputHandler.vertical;
+            targetDirection += cameraObject.right * inputHandler.horizontal;
+
+            targetDirection.Normalize();
+            targetDirection.y = 0;
+
+            if(targetDirection == Vector3.zero)
+                targetDirection = myTransform.forward;
+
+            float rs = rotationSpeed * moveOverride;
+            
+            Quaternion tr = Quaternion.LookRotation(targetDirection);
+            Quaternion targetRotation = Quaternion.Slerp(myTransform.rotation, tr, rs * delta);
+
+            myTransform.rotation = targetRotation;
+        }
+
+        #endregion
     }
 }
