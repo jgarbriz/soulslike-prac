7eb17fff502c2c702570a6ae98c8ea7f23a478b7
Juan García <garciabrizjuan@gmail.com>
2022-05-08 15:26:44 +0200

    Added walking and running animations

--- Assets/Scripts/PlayerLocomotion.cs
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -11,6 +11,7 @@ namespace SL
         Vector3 moveDirection;
 
         [HideInInspector] public Transform myTransform;
+        [HideInInspector] public AnimatorHandler animatorHandler;
 
         public Rigidbody rigidBody;
         public GameObject normalCamera;
@@ -23,8 +24,11 @@ namespace SL
         {
             rigidBody = GetComponent<Rigidbody>();
             inputHandler = GetComponent<InputHandler>();
+            animatorHandler = GetComponentInChildren<AnimatorHandler>();
             cameraObject = Camera.main.transform;
             myTransform = transform;
+
+            animatorHandler.Initialize();
         }
 
         void Update()
@@ -42,10 +46,15 @@ namespace SL
 
             Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
             rigidBody.velocity = projectedVelocity;
+
+            animatorHandler.UpdateAnimatorValues(inputHandler.moveAmount, 0);
+
+            if (animatorHandler.canRotate) {
+                HandleRotation(delta);
+            }
         }
 
         #region Movement
-        
         Vector3 normalVector;
         Vector3 targetPosition;
 
@@ -60,17 +69,17 @@ namespace SL
             targetDirection.Normalize();
             targetDirection.y = 0;
 
-            if(targetDirection == Vector3.zero)
+            if (targetDirection == Vector3.zero) {
                 targetDirection = myTransform.forward;
+            }
 
             float rs = rotationSpeed * moveOverride;
-            
+
             Quaternion tr = Quaternion.LookRotation(targetDirection);
             Quaternion targetRotation = Quaternion.Slerp(myTransform.rotation, tr, rs * delta);
 
             myTransform.rotation = targetRotation;
         }
-
         #endregion
     }
 }
